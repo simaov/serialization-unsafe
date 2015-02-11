01b149f7057ab1ec445da6e419f99905c105e032
simaov <simonchuk.ov@gmail.com>
2015-02-11 11:51:11 +0200

    use lock instead synchronized

--- src/main/java/org/jcoffee/serialization/SerializerFactory.java
+++ src/main/java/org/jcoffee/serialization/SerializerFactory.java
@@ -2,20 +2,28 @@ package org.jcoffee.serialization;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class SerializerFactory {
 
     private static final Map<Class, SerializerUnsafeI> SERIALIZER_UNSAFE_MAP = new HashMap<>();
 
-    private SerializerFactory() {}
+    private static final Lock LOCK = new ReentrantLock();
+
+    private SerializerFactory() {
+    }
 
     @SuppressWarnings("unchecked")
     public static <T> SerializerUnsafeI<T> getSerializer(Class<T> clazz) {
         if (!SERIALIZER_UNSAFE_MAP.containsKey(clazz)) {
-            synchronized (SERIALIZER_UNSAFE_MAP) {
+            try {
+                LOCK.lock();
                 if (!SERIALIZER_UNSAFE_MAP.containsKey(clazz)) {
                     SERIALIZER_UNSAFE_MAP.put(clazz, new SerializerUnsafe<>(clazz));
                 }
+            } finally {
+                LOCK.unlock();
             }
         }
         return SERIALIZER_UNSAFE_MAP.get(clazz);
